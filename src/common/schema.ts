81f5e6345c24551d6f2f2a65f48b0c3cc055a206
RayhanHamada <rayhanhamada1999@gmail.com>
2022-05-17 00:40:07 +0700

    feat: add common schema

---
+++ src/common/schema.ts
@@ -0,0 +1,41 @@
+import { Type } from '@sinclair/typebox';
+
+export const DefaultResponse204Schema = Type.Object(
+  {},
+  {
+    description: 'Success. No Content',
+  }
+);
+
+export const DefaultResponse404Schema = Type.Object(
+  {
+    statusCode: Type.Literal(404),
+    error: Type.Literal('Not Found'),
+    message: Type.String(),
+  },
+  {
+    description: 'Item Not Found',
+  }
+);
+
+export const DefaultResponse400Schema = Type.Object(
+  {
+    statusCode: Type.Literal(400),
+    error: Type.Literal('Bad Request'),
+    message: Type.String(),
+  },
+  {
+    description: 'Bad Request',
+  }
+);
+
+export const DefaultResponse415Schema = Type.Object(
+  {
+    statusCode: Type.Literal(415),
+    error: Type.Literal('Unsupported Media Type'),
+    message: Type.String(),
+  },
+  {
+    description: 'Bad Request',
+  }
+);
