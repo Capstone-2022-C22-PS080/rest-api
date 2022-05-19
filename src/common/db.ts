db675afe45cb89913fbaf0f6645b6f6c25496323
RayhanHamada <rayhanhamada1999@gmail.com>
2022-05-20 01:11:42 +0700

    feat: add db url fetching for production

--- src/common/db.ts
+++ src/common/db.ts
@@ -1,5 +1,15 @@
 import { PrismaClient } from '@prisma/client';
+import { dbURL } from './constants';
 
-const db = new PrismaClient();
+const db = new PrismaClient({
+  datasources:
+    process.env.NODE_ENV === 'development'
+      ? undefined
+      : {
+          db: {
+            url: dbURL,
+          },
+        },
+});
 
 export default db;
