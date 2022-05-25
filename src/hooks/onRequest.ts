212ad242f5115907d01999f7376485b26cf7fff6
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2022-05-25 15:39:01 +0700

    feat: add jwt auth

--- src/hooks/onRequest.ts
+++ src/hooks/onRequest.ts
@@ -1,6 +1,16 @@
 import { onRequestHookHandler } from 'fastify';
 
 const onRequest: onRequestHookHandler = async (req, res) => {
+  const publiclyAuthorizedPath = ['/token', '/static', '/json'];
+
+  if (
+    req.routerPath.includes('token') ||
+    req.routerPath.includes('static') ||
+    req.routerPath.includes('json')
+  ) {
+    return;
+  }
+
   try {
     await req.jwtVerify();
   } catch (err) {
